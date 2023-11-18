dff18734f0185800b7cfe0265da3dfbd5f04b789
alexguajardoc <alexguajardoc0902@gmail.com>
2023-11-18 16:41:46 -0600

    Comments

--- entregable3.py
+++ entregable3.py
@@ -35,7 +35,7 @@ radioCurvatura = lambda x: ((1.0 + primderiv(x)**2) ** 1.5) / np.abs(segderiv(x)
 # Regresa la pendiente de la recta tangente en cualquier punto (pero se usa para la zona de riesgo)
 pendiente = lambda x: 3*a3*(x**2) + 2*a2*x + a1
 
-''' Zonas de Derrape '''
+''' Zona de Derrape '''
 
 zonaRiesgo = []
 zonaRiesgoCurv = []
@@ -48,8 +48,11 @@ for i in range(500):
         zonaRiesgoCurv.append(rC_act)
         zonaRiesgo.append(a + dx*i)
 
-print(f"Radio de Curvatura en zona de riesgo: {zonaRiesgoCurv[::]}")
-print(f"\n\n\Rango de puntos de Zona de Riesgo: {zonaRiesgo[0]}, {zonaRiesgo[-1]} ")
+# Validación del radio de curvatura en la zona de riesgo
+#print(f"Radio de Curvatura en zona de riesgo: {zonaRiesgoCurv[::]}")
+
+### Obtención de la Zona de Riesgo
+print(f"\nRango de puntos en x de la Zona de Riesgo: {zonaRiesgo[0]}, {zonaRiesgo[-1]}\n")
 
 ## Graficar la función cúbica obtenida
 
@@ -59,6 +62,8 @@ y_valoresPol = funcion(x_valoresPol)
 
 # Graficar estos valores como líneas usando matplotlib
 plt.plot(x_valoresPol,y_valoresPol, "-k",linewidth = 10, label = "Curva Generada")
+
+## Graficar la zona de riesgo
 
 # Declarar un linspace de la zona de riesgo y sus valores de y correspondientes
 x_valores = np.linspace(zonaRiesgo[0],zonaRiesgo[-1],len(zonaRiesgo))
@@ -70,10 +75,10 @@ for i in range(len(zonaRiesgo)):
 # Graficar estos valores como una línea azul usando matplotlib
 plt.plot(x_valores,y_valores[::], color="#FFEA00", linewidth=10.6, label = "Zona de Derrape/Riesgo")
 
-# Graficar línea punteada con los valores de antes
+# Graficar línea punteada con los valores de antes para que parezca calle
 plt.plot(x_valoresPol,y_valoresPol, "--w", linewidth = 1.5)
 
-# Graficar la recta tangente en cualquier punto de la zona de riesgo
+## Graficar la recta tangente en la zona de riesgo
 i = zonaRiesgo[0]
 j = zonaRiesgo[-1]
 
@@ -86,14 +91,17 @@ y02 = funcion(j)
 rangox = np.linspace(x0 - 10, x0 + 80)
 rangox2 = np.linspace(x02 - 10, x02 + 80)
 
+# Función de la recta tangente
 linea = lambda rangox, x0, y0: pendiente(x0)*(rangox-x0) + y0
 
+# Valor que determina la longitud de las rayas punteadas, correspondiente al parámtero "dashes"
 rayas = [2,2]
 
-plt.gca().plot(rangox, linea(rangox,x0,y0), 'c--', linewidth = 1.5, dashes = rayas, label = "Recta Tangente Inicial")
-plt.gca().plot(rangox2, linea(rangox2,x02,y02), 'm--', linewidth = 1.5, dashes = rayas, label = "Recta Tangente Final")
+# En los ejes actuales, graficar: el rango de x como valores de x, aquellos evaluados en la función de la recta tangente como y.
+plt.gca().plot(rangox, linea(rangox,x0,y0), 'c--', linewidth = 1.5, dashes = rayas, label = "Recta Tangente Inicial") # Color cián
+plt.gca().plot(rangox2, linea(rangox2,x02,y02), 'm--', linewidth = 1.5, dashes = rayas, label = "Recta Tangente Final") # Color magenta
 
-# Gradas de 80m x 10m
+### Gradas de 80m x 10m
 grada1 = plt.Rectangle([147.7,353.7], -80, 10, angle = 27, rotation_point = "xy")
 grada2 = plt.Rectangle([227,267.1], 80, 10, angle = -73, rotation_point = "xy")
 plt.gca().add_patch(grada1)
